207043dede243f677b89ef83c1c164a8dfa17657
saadriz123 <saadrizan0521@gmail.com>
2024-09-13 17:13:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/Medicare/sw.js')
+        .then(registration => {
+          console.log('Service Worker registered with scope:', registration.scope);     
+        }, err => {
+          console.log('Service Worker registration failed:', err);
+        });
+    });
+  }
